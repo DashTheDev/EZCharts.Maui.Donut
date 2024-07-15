8ad4971b850358950b4eaf6f39408858d01b9656
DashTheDev <myersdashiel@gmail.com>
2024-07-15 17:48:02 +1000

    Slight clean up.

---
+++ EZCharts.Maui.Donut/Models/Defaults.cs
@@ -0,0 +1,31 @@
+﻿namespace EZCharts.Maui.Donut.Models;
+
+internal static class Defaults
+{
+    internal static readonly Thickness Padding = new(10);
+    internal const float ChartRotationDegrees = 90f;
+    internal const float ChartOuterRadius = 250f;
+    internal const float ChartInnerRadius = 125f;
+
+    internal const LabelStyle MainLabelStyle = LabelStyle.Key;
+    internal static readonly Color LabelFontColor = Colors.White;
+    internal const bool LabelUseAutoFontColor = false;
+    internal const string LabelFontFamily = "Arial";
+    internal const float LabelFontSize = 20f;
+    internal const float LabelKeySpacing = 10f;
+    internal const float LabelKeyColorOffset = 20f;
+    internal const float LabelOutsideRadius = 50f;
+
+    internal const string EntryValuePath = "Value";
+    internal const string EntryLabelPath = "Label";
+    internal const float EntryImageScale = 0.1f;
+
+    internal static readonly Color[] ChartColors =
+    [
+        Colors.Blue,
+        Colors.Green,
+        Colors.Yellow,
+        Colors.Purple,
+        Colors.Orange
+    ];
+}
